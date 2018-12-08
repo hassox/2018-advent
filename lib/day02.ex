bf3c3f9fc3b73cc06417900f0ee1367e0fafd177
Daniel Neighman <dneighman@gmail.com>
2018-12-08 12:56:31 -0800

    Complete day 2 solution

--- lib/day02.ex
+++ lib/day02.ex
@@ -262,11 +262,26 @@ defmodule Advent2018.Day02 do
   end
 
   def two do
+    two(@input)
+  end
+
+  # entry
+  def two([id | rest]),
+    do: two(id, rest)
+
+  # end no match
+  def two(_, []),
+    do: {:error, :no_match}
+
+  def two(id1, [id2 | rest] = targets) do
+    case find_matching_box(id1, targets) do
+      nil -> two(id2, rest)
+      box_id -> box_id
+    end
   end
 
   defp find_dup_tokens(id) do
     map = map_token_counts(id)
-    IO.inspect(map)
     vals = Map.values(map)
     twos = if 2 in vals, do: 1
     threes = if 3 in vals, do: 1
@@ -280,4 +295,31 @@ defmodule Advent2018.Day02 do
       Map.update(acc, c, 1, fn n -> n + 1 end)
     end)
   end
+
+  defp find_matching_box(_, []),
+   do: nil
+
+  defp find_matching_box(id1, [id2 | rest]) do
+    letters1 = String.graphemes(id1)
+    letters2 = String.graphemes(id2)
+    case compare_boxes(letters1, letters2, 0, []) do
+      nil -> find_matching_box(id1, rest)
+      box_id -> box_id
+    end
+  end
+
+  defp compare_boxes(_, _, diffs_so_far, _) when diffs_so_far > 1, do: nil
+
+  # letters are the same
+  defp compare_boxes([a | rest1], [a | rest2], diffs_so_far, letters_so_far),
+    do: compare_boxes(rest1, rest2, diffs_so_far, [a | letters_so_far])
+
+
+  # letters are different
+  defp compare_boxes([_ | rest1], [_ | rest2], diffs_so_far, letters_so_far),
+    do: compare_boxes(rest1, rest2, diffs_so_far + 1, letters_so_far)
+
+  # we've got to the end and we're good... found a match!
+  defp compare_boxes([], [], _, letters),
+    do: letters |> Enum.reverse() |> Enum.join("")
 end
